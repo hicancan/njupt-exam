aeb52910fb51192540e519a60cb936e6be525911
hicancan <hicancan@qq.com>
2026-05-21 21:18:03 +0800

    UI: Remove redundant fake breadcrumb from ExamDetail page

--- src/components/ExamDetail.tsx
+++ src/components/ExamDetail.tsx
@@ -58,18 +58,6 @@ export function ExamDetail({
 
     return (
         <div className="fade-in max-w-[652px] pb-10">
-            <div className="flex items-center gap-2 mb-1.5">
-                <div className="bg-[#f1f3f4] dark:bg-[#303134] rounded-full p-1">
-                    <svg className="w-3 h-3 text-[#5f6368] dark:text-[#bdc1c6]" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
-                        <path fillRule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clipRule="evenodd" />
-                    </svg>
-                </div>
-                <span className="text-[14px] text-[#202124] dark:text-[#dadce0] font-normal truncate">
-                    {APP_CONFIG.DOMAIN} <span className="text-[#5f6368] dark:text-[#9aa0a6]">› detail › {className}</span>
-                </span>
-            </div>
-            
             <div className="flex flex-col sm:flex-row sm:items-start justify-between gap-4 mb-6">
                 <div>
                     <h2 className="text-[28px] font-normal text-[#202124] dark:text-[#e8eaed] leading-tight mb-2">
