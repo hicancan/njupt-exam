a0e777e36850aed8c7585b0938c80953c615c0dd
hicancan <hicancan@qq.com>
2026-05-22 11:39:48 +0800

    feat: use pure gradle build by committing bubblewrap android wrapper

---
+++ app/src/main/java/top/hicancan/njupt/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package top.hicancan.njupt.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
